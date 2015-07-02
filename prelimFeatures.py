47248a14ce917cfc1729d01d2973a62fb50c3e9e
Ritesh Shah <riteshshah@pc-shah.imag.fr>
2015-07-03 01:32:02 +0200

    Initial commit

---
+++ prelimFeatures.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+"""
+Module Docstring
+Docstrings: http://www.python.org/dev/peps/pep-0257/
+"""
+
+__author__ = 'Ritesh Shah (ritesh.shah@imag.fr)'
+__copyright__ = 'Copyright (c) 2014-2015 Ritesh Shah'
+__license__ = 'New-style BSD'
+__vcs_id__ = '$'
+__version__ = '1.2.3'  #Versioning: http://www.python.org/dev/peps/pep-0386/
+
+### Imports ###
+
+### Begin code ###
+
+def test():
+    """ Testing Docstring"""
+    pass
+
+if __name__=='__main__':
+    test()
+
+
+def featCommaSepList():
+    """
+    Feature to capture single tokens separated by atleast two commas.
+    For instance:
+        "delhi , mumbai , jaipur"
+        "delhi , mumbai , chandigarh , gandhinagar"
+    Args:
+        normText: sequence of normalised tokens (where token, single whitespace separated character sequence)
+
+    Returns:
+
+    """
+
+
+if __name__ == '__main__':
+    test()
+
